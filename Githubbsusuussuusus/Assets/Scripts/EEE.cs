1195738de490d0b47b2997b0719d7d4c21bc1c32
koishei swag <87334785+SandwitchAssasin@users.noreply.github.com>
2022-11-04 18:41:25 +0100

    WpierwOmuwicPlan
    KoniecTestuwIthink

--- Githubbsusuussuusus/Assets/Scripts/EEE.cs
+++ Githubbsusuussuusus/Assets/Scripts/EEE.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EEE : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void Statto()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("EEE");
+        SceneManager.LoadScene(1);
+        //starts gaem
     }
 }
