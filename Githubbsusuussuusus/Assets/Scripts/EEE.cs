51ac73015d15a190647ee4906a2efc13c9322ab6
koishei swag <87334785+SandwitchAssasin@users.noreply.github.com>
2022-11-19 19:08:50 +0100

    druga pruvaa
    kkhta4life

--- Githubbsusuussuusus/Assets/Scripts/EEE.cs
+++ Githubbsusuussuusus/Assets/Scripts/EEE.cs
@@ -5,14 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class EEE : MonoBehaviour
 {
+    public GameObject mainMenu;
     public GameObject credits;
     public GameObject chooseLevel;
+    public AudioSource aS;
+    public AudioClip[] ac;
     public int diff;
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        //when getting back destroy this;
+    }
     public void Statto(int diff_)
     {
         diff = diff_;
         Debug.Log(diff);
         SceneManager.LoadScene(1);
+        aS.clip = ac[diff + 1];
+        aS.Play();
+        mainMenu.SetActive(false);
+        chooseLevel.SetActive(false);
         //starts gaem
     }
     public void Choose()
